1fbf00e443d443ef86edb656b293d0c37434aa1f
Vladimir RM <shvdrummer@gmail.com>
2022-11-25 13:56:34 +0200

    crate span.appevhild

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ var myNodeList = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodeList.length; i++) {
   var span = document.createElement("span");
-  var txt = document.createTextNode("u00D7");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild;
+  myNodeList[i].appendChild(span);
 }
